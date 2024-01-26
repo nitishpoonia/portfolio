133d73f02e71ca21d346f04c29f10e6a6ba776a0
Nitish Poonia <npoonia39@gmail.com>
2024-01-26 22:34:34 +0530

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "./styles/Container.styled";
+import { Project } from "./styles/Project.styled";
+function Projects() {
+  return (
+      <Container>
+          <h1>Projects</h1>
+          <Project>
+              <h1>
+                  
+              </h1>
+          </Project>
+          
+    </Container>
+  );
+}
+
+export default Projects;
---
+++ src/components/styles/GlobalStyle.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: inherit;
+    }
+
+    /* Body styles */
+    body {
+        font-family: Lato, sans-serif;
+        color: #333333;
+        background-color: #F5F5F5;
+    }
+
+    /* Font styles */
+    h1,
+    h2,
+    h3 {
+        font-family: Roboto Bold, sans-serif;
+        font-weight: bold;
+        margin-bottom: 1rem;
+    }
+
+    p {
+        line-height: 1.5;
+    }
+
+    a {
+        color: #007AFF;
+        text-decoration: none;
+        transition: color 0.2s ease-in-out;
+    }
+
+    a:hover,
+    a:active {
+        color: #E5E5E5;
+    }
+
+    /* Accent styles */
+    .accent {
+        color: #007AFF;
+        font-family: Playfair Display, serif;
+    }
+
+    /* Additional styles you might need */
+    /* ... */
+`;
+
+export default GlobalStyle;
